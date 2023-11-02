478ca31d8b5cd398e61353134f5b9150ec0b575e
MORIOKA Tomohiko <tomo.git@chise.org>
2023-11-02 17:12:55 +0900

    New file.

---
+++ index.js
@@ -0,0 +1,42 @@
+async function get_hanzi_products (character) {
+  const ipfs_gw_URL = "http://localhost:8080";
+  //const ipfs_gw_URL = "https://www.chise.org";
+  //const ipfs_gw_URL = "https://ipfs.io";
+  const requestURL = ipfs_gw_URL + "/ipns/chise.org/v1/character/a/ucs/default/0x"
+    + character.codePointAt(0).toString(16).toUpperCase() + "/ideographic-products.json";
+  //console.log(requestURL);
+  const request = new Request(requestURL);
+
+  const response = await fetch(request);
+  const hanzi_products = await response.json();
+
+  return hanzi_products;
+}
+
+// get_hanzi_products ('字').then((v) => { console.log (v); });
+
+
+function intersection (sets) {
+  let set0 = sets[0];
+
+  for (const set of sets) {
+    set0 = set0.filter((x) => set.includes(x));
+  }
+  return set0;
+}
+
+// p1 = get_hanzi_products ('木');
+// p2 = get_hanzi_products ('口');
+// Promise.all([p1, p2])
+//   .then(([a, b]) => {
+// 	  console.log (a.filter((val_a) => b.includes(val_a)));
+// 	});
+
+
+async function ids_find (queries) {
+  //const queries = ['一', '口', '土', '田'];
+
+  // 並行に非同期処理を実効
+  const ret = await Promise.all(queries.map(get_hanzi_products));
+  return intersection (ret);
+}
